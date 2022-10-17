5e2fc27a3550ff7d9e61b02ca6d62eaa93e0370d
Dilshodbek <dilshodtuit217@gmail.com>
2022-10-18 04:02:05 +0500

    add for()

--- For/script.js
+++ For/script.js
@@ -1,1 +1,96 @@
 "use strict";
+
+// 1-masala
+
+// let k = 5;
+// let n = 7;
+// for (let i=1; i<=n; i++) {
+//     console.log(i);
+// }
+
+// 2-masala
+
+// let a = 15;
+// let b = 7;
+// for (let i=b; i<=a; i++) {
+//     console.log(i);
+// }
+
+// 3-masala
+
+// let a = 15;
+// let b = 7;
+// for (let i=14; i>b; i--) {
+//     console.log(i);
+// }
+
+// 4-masala
+
+// let a = 10;
+// let b = 10000;
+// for (let i=1; i<=a; i++) {
+//     let c = i * b;
+//     console.log(c);
+// }
+
+// 5-masala
+
+// let a = 10;
+// let b = 10000;
+// for (let i=1; i<=a; i++) {
+//     let c = i * b * 0.1;
+//     console.log(c);
+// }
+
+// 6-masala
+
+// let a = 1;
+// let b = 2;
+// let c = 10000;
+// for (let i=1; i<=b; i=i+0.2) {
+//     console.log(Math.round(i*c));
+// }                               
+
+// 7-masala
+
+// let a = 1;
+// let b = 10;
+// let c = 0;
+// if (a%1 ==0) {
+//     for(let i=a; i<=b; i++) {
+//         c+=i
+//     }
+// }
+// console.log(c);
+
+// 8-masala
+
+// let a = 1;
+// let b = 10;
+// let c = 1;
+// if (a%1 == 0) {
+//     for(let i=a; i<=b; i++) {
+//         c*=i
+//     }
+// }
+// console.log(c);
+
+// 9-masala
+
+// let a = 1;
+// let b = 10;
+// let c = 0;
+// if (a%1 ==0) {
+//     for(let i=a; i<=b; i++) {
+//         c = c + i**2;
+//     }
+// }
+// console.log(c);
+
+// 10-masala
+
+// let a = 7;
+// let c = 0;
+// for (let i=1; i<=a; i++) {
+//     c = c + 1/i;
+// }   console.log(c);
